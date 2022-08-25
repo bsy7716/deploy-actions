e1dd317af39c59db4877e50ed6714e4b32906a2b
bsy7716 <bsy7716@naver.com>
2022-08-25 17:26:35 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React : Final Deploy Actions
+          Learn React : Final Deploy Actions2
         </a>
       </header>
     </div>
